9d19cc0e9f32e1e8cab6c1f5523caf7d4a165d74
Andres G <150415960+aztro09@users.noreply.github.com>
2024-06-30 12:26:38 -0600

    Update and rename validWords.java to ValidWords.java

--- parserBottomUp/src/com/myself/miParser/ValidWords.java
+++ parserBottomUp/src/com/myself/miParser/ValidWords.java
@@ -3,28 +3,27 @@ package com.myself.miParser;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Stack;
 
-public class validWords{
+public class ValidWords {
     private Set<String> validNombres;
     private Set<String> validAdjetivos;
     private Set<String> validSujeto;
 
-    public validWords(){
+    public ValidWords() {
         initializeValidWords();
     }
 
-    private void initializeValidWords(){
+    private void initializeValidWords() {
         validNombres = new HashSet<>(Arrays.asList("Juan", "Maria", "perro", "gato"));
         validAdjetivos = new HashSet<>(Arrays.asList("grande", "pequeño", "rojo", "azul"));
         validSujeto = new HashSet<>(Arrays.asList("yo", "tu", "el", "ella", "nosotros"));
     }
 
-    public Set<String> getValidNombres(){
+    public Set<String> getValidNombres() {
         return validNombres;
     }
 
-    public Set<String> getValidAdjetivos(){
+    public Set<String> getValidAdjetivos() {
         return validAdjetivos;
     }
 
